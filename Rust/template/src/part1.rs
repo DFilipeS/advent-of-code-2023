0177b51583e225d4a1d69da6345341fc73b08a08
Daniel Silva <danfilipesilva@gmail.com>
2023-12-10 17:58:40 +0000

    Added template project.

---
+++ Rust/template/src/part1.rs
@@ -0,0 +1,3 @@
+pub fn process(input: &str) -> String {
+    todo!("part 1 not implemented yet")
+}
---
+++ Rust/template/src/part2.rs
@@ -0,0 +1,3 @@
+pub fn process(input: &str) -> String {
+    todo!("part 2 not implemented yet")
+}
